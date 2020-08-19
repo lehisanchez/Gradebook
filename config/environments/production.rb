03f13187f0d5ff8c0eb0f850a38b6448d6f1d815
Lehi Sanchez <sanchez.lehi@gmail.com>
2020-08-19 11:41:58 -0400

    Updated Production cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,10 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store
+  
+  # Stimulus Reflex
+  config.session_store :cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
